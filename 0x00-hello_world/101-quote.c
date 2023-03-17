47c26c61c4d956c1d4be3cb6607ffc51ed951d70
Ladyroyale <temilayomathew@gmail.com>
2023-03-17 14:51:58 -0700

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - print without using printf or puts function
- *
  * Return: 1 always (success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora korpar,
-2015-10-19"\n, stdout");
-return (1);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
