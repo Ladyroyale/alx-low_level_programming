24c3df2573753590ace7d342b35ae486794cf4f4
Ladyroyale <temilayomathew@gmail.com>
2023-03-16 19:22:15 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,13 @@
 /**
  * main - print without using printf or puts function
  *
- * Return: 1 always when successful
+ * Return: 1 always (success)
  */
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora korpar,
+2015-10-19"\n, stdout);
+return (1);
+
 }
