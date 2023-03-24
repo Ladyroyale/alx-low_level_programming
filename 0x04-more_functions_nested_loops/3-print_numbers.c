3f04315dbc393ad175e060d0cef904ccfef5144f
Ladyroyale <temilayomathew@gmail.com>
2023-03-24 08:58:53 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - function that prints number 0 to 9
+ * Return: 0 always
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+		_putchar(c);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,4 +1,4 @@
-#innclude < unistd.h >
+#include <unistd.h>
 
 /**
  * _putchar - writes the character c to stdout
