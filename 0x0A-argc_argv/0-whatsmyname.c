204a8a66d5ed8b10117e3fde7e1e49d8df4c9d40
Ladyroyale <temilayomathew@gmail.com>
2023-04-09 13:49:42 -0700

    rename

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program that prints its name
+ * @rgc: parameter
+ * @argv: string to be renamed
+ * Return: 0 always 
+ */
+int main(int argc, char *s)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
