44aba436791437e3d2d702b61f4f1120236c75d0
Ladyroyale <temilayomathew@gmail.com>
2023-04-10 06:44:55 -0700

    rename

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,7 @@
  * @argv: string to be renamed
  * Return: 0 always
  */
-int main(int argc, char *argv[])
+int main(int argc _attribute_((unused)),  char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
