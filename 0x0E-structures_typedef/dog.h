2c51b1b0b06d94c8912090596fc8a5306cf4b890
Ladyroyale <temilayomathew@gmail.com>
2023-04-17 17:42:10 -0700

    dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+
+
+
+
+#endif
