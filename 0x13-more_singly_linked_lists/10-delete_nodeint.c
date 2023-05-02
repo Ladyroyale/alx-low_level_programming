2481a98542fcaaf195fa33076a0a057e953a5458
Ladyroyale <temilayomathew@gmail.com>
2023-05-02 16:54:08 -0700

    reverse

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
@@ -27,6 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		list = list->next;
 		j++;
 	}
+
 	point = list->next;
 	list->next = point->next;
 	free(point);
