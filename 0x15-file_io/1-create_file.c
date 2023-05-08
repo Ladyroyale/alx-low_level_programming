85256f4b0f0a9a0f7d4059c5650a20380d2af771
Ladyroyale <temilayomathew@gmail.com>
2023-05-08 15:49:28 -0700

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,11 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (wen = 0; text_content[wen];)
-		       wen++;
+			wen++;
 	}
 
 	tf = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	m = write(tf, text_content, wen);
 
 	if (tf == -1 || m == -1)
